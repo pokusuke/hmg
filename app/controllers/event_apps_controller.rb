e4efffab729114de90079e7cf86999ca07a5960a
pokusuke <yosuke@hunyago-web-garden.com>
2019-09-16 02:12:41 +0900

    応募取り消し機能追加

--- app/controllers/event_apps_controller.rb
+++ app/controllers/event_apps_controller.rb
@@ -1,5 +1,5 @@
 class EventAppsController < ApplicationController
-  before_action :authenticate_user!, only:[:index,:create]
+  before_action :authenticate_user!, only:[:index,:create,:destroy]
   def index
     @event_apps = EventApp.where(user_id: current_user.id)
   end
@@ -9,6 +9,8 @@ class EventAppsController < ApplicationController
     event_app.event_id = params[:event][:event_id]
     event_app.user_id = current_user.id
     event_app.save!
+    flash[:success] = 'イベントに応募しました'
+    redirect_to event_app_path(event_app)
   end
 
   def show
@@ -16,5 +18,12 @@ class EventAppsController < ApplicationController
     if @event_app.user_id != current_user.id
       redirect_to event_apps_path
     end
+  end
+
+  def destroy
+    event_app = EventApp.find(params[:id])
+    event_app.destroy
+    flash[:success]='応募を取消ました'
+    redirect_to event_apps_path
   end
 end

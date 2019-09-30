e2b1a2c70a56823b8f135c7aab6b8f4c488f981d
pokusuke <yosuke@hunyago-web-garden.com>
2019-09-30 23:52:50 +0900

    admin権限機能の追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,28 +1,4 @@
 module SessionsHelper
-  #  def log_in(user)
-  #    session[:user_id] = user.id
-  #  end
-
-  #  def current_user
-  #    if(user_id = session[:user_id])
-  #      @current_user ||= User.find_by(id: user_id)
-  #    end
-  #  end
-
-  #  def forget(user)
-  #    #user.forget
-  #    cookies.delete(:user_id)
-  #  end
-
-  # def log_out
-  #  forget(current_user)
-  #   session.delete(:user_id)
-  #   @current_user = nil
-  # end
-
-  # def logged_in?
-  #  !current_user.nil?
-  # end
 
   def is_apply_event?
     cid = current_user
@@ -31,5 +7,9 @@ module SessionsHelper
     else
       !!EventApp.find_by(user_id: cid, event_id: params[:id])
     end
+  end
+  
+  def admin_user                                                           
+    redirect_to(root_url) unless current_user.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_150143) do
+ActiveRecord::Schema.define(version: 2019_09_30_143351) do
 
   create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "area_name"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2019_09_15_150143) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

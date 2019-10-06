5d9ebd93e0831fd622b4455c5548e4aea1cfa5f7
pokusuke <yosuke@hunyago-web-garden.com>
2019-10-06 23:56:31 +0900

    アップロード画像保存先をS3にする設定追加

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = Rails.application.credentials.aws[:backet]
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: Rails.application.credentials.aws[:region],
+      path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/

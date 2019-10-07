a0013cd5717756f8ae57575d616bd6bb02daf2c1
pokusuke <yosuke@hunyago-web-garden.com>
2019-10-07 18:52:40 +0900

    不要なコメントアウトを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,37 +2,22 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'chat/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'events#index'  
-  #get '/users',to:'users#index'
-  #get '/users/new',to:'users#new'
-  #post '/users',to:'users#create'
-  #get '/users/:id/edit',to:'users#edit'
+ 
   resources :users
   resources :areas
   resources :prefs
   resources :events
   resources :event_apps do
     resources :stores
-  #  resources :event_msgs
   end
-  #get '/login', to:'sessions#new'
-  #post '/login',to:'sessions#create'
+
   delete '/logout',to:'sessions#destroy'
-
-  #post '/event_apps',to:'event_apps#create'
-  #get '/event_apps',to:'event_apps#index'
-  #get '/event_apps/:id',to:'event_apps#show',as: 'event_app'
-
 
   get '/event_msgs',to:'event_msgs#index'
   post '/event_msgs',to:'event_msgs#create'
 
-  #get '/stores',to:'stores#index'
-  #post '/stores',to:'stores#create'
-  #get '/stores/new',to:'stores#new'
-  #post '/stores/store_edit',to:'stores#store_edit'
-  #get '/stores/:id', to:'stores#show'
 
   get '/event_msg_reps/new',to:'event_msg_reps#new'
   post '/event_msg_reps',to:'event_msg_reps#create'
